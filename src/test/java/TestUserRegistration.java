7971ae00fdb84bd486d71c2ed74ee50a7712539c
sandipk01 <sandipkengar01@gmail.com>
2020-03-05 17:17:16 +0530

    ADDED : FirstName Validate Test Case

--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestUserRegistration {
+    //validating first name initial capital letter and at least 3 character should enter
+    @Test
+    public void givenFirstName_WhenValid_ThenTrue() {
+        boolean result=UserRegistration.isFirstNameInitialCapital("Sandip");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenFirstName_WhenInvalid_ThenFalse() {
+        boolean result=UserRegistration.isFirstNameInitialCapital("sandip");
+        Assert.assertFalse(result);
+    }
+
 }
