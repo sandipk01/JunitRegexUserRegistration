a8e753adf85db92b9c758af1bbb2c5d7942e5a54
sandipk01 <sandipkengar01@gmail.com>
2020-03-06 00:24:59 +0530

    ADDED : isMobileNumberValidate method is added

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -14,6 +14,10 @@ public class UserRegistration {
     public static boolean isEmailValidate(String emailAddress){
         return validator("^[a-z]{1,}([.]?[-]?[+]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]?[a-z]{2,4})?$",emailAddress);
     }
+    //validating mobile number with two digit country code one space and 10 digit number
+    public static boolean isMobileNumberValidate(String mobileNumber){
+        return validator("^[0-9]{2}[ ]{1}[0-9]{10}$",mobileNumber);
+    }
     //common validation function
     public static boolean validator(String validationPattern,String input)
     {
@@ -37,5 +41,6 @@ public class UserRegistration {
         System.out.println("First Name is : " + result(isFirstNameInitialCapital("Sandip")));
         System.out.println("Last Name is :"+result(isLastNameInitialCapital("Kengar")));
         System.out.println("Email Address is : "+result(isEmailValidate("sandipk01@gmail.com")));
+        System.out.println("Mobile Number is : "+result(isMobileNumberValidate("91 8857458747")));
     }
 }
